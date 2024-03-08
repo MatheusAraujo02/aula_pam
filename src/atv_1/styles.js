f4ca403dd9942ab89733099ab253e539fbc1523a
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-03-07 22:28:08 -0300

    atv_1_3

--- src/atv_1/styles.js
+++ src/atv_1/styles.js
@@ -3,30 +3,32 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
        flex: 1,
-       width: '100%',
+       width: '50%',
        borderWidth: 1,
        justifyContent: 'center',
        backgroundColor: '#E2F2FD',
        padding: 10,
        marginBottom: 8,
+       marginTop: 8,
        alignItems: 'center',
     },  
     paragraph: {
-        margin: 24,
+        margin: 15,
         fontSize: 18,
+        fontSize: 25,
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#1565C0',
     },
     image: {
-        width: '90%',
+        width: '100%',
         height: '15%',
         margin: 10,
     },
     texto: {
         fontSize: 25,
        textAlign: 'center',
-       padding: 8,
+       padding: 10,
     }
 });
 
