518d9ffeed3f8e4b3a11b0362bf3103632189053
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-05-15 22:46:56 -0300

    ex_6 concluido

--- src/ex_6/botao/index.js
+++ src/ex_6/botao/index.js
@@ -2,16 +2,18 @@ import { Pressable, Text } from "react-native";
 
 import styles from "./styles";
 
-export default function Botao ({children}) {
+export default function Botao ({children, calcular}) {
     return (
         <Pressable 
             style=
-            {({ pressed }) => pressed ? 
-            [styles.botao, styles.btnPress]
-            :
-            styles.botao
-        }
-        >
+                {({ pressed }) => pressed ? 
+                    [styles.botao, styles.btnPress]
+                :
+                    styles.botao
+            }
+
+            onPress={() => calcular()}
+        >   
             <Text style={styles.txtBotao}> {children} </Text>
         </Pressable>
     )
--- src/ex_6/index.js
+++ src/ex_6/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { View, Text } from 'react-native';
 
 import styles from './styles';
@@ -5,15 +6,26 @@ import Input from './input';
 import Botao from './botao';
 
 export default function Exemplo_6 (){
+
+    const [peso, setPeso] = useState(null);
+    const [altura, setAltura] = useState(null);
+    const [imc, setImc] = useState(0.00);
+
+    function calculaImc () {
+        setImc(peso / (altura*altura));
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.titulo}> Exemplo 6</Text>
             <View style={styles.ladoalado}>
-                <Input placeholder='Peso'  />
-                <Input placeholder='Altura'  />
+                <Input placeholder='Peso' valor={peso} atualizaValor={setPeso}  />
+                <Input placeholder='Altura' valor={altura} atualizaValor={setAltura}  />
             </View>
 
-            <Botao>Calcular</Botao>
+            <Text style={styles.imc}>{imc.toFixed(2)}</Text>
+
+            <Botao calcular={calculaImc}>Calcular</Botao>
         </View>
 
     )
